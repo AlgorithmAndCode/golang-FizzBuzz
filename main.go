e4d21eaf37a3827857897875dff2a2fe6875b9c7
Marcos <velozmarkdrea@gmail.com>
2017-01-08 15:40:50 +0100

    Fizz case encapsulating types

--- main.go
+++ main.go
@@ -5,9 +5,30 @@ import (
 	"strconv"
 )
 
-func FizzBuzz(number float64) string {
+type FizzBuzzType interface {
+	result(number float64) string
+}
+
+type Fizz struct {
+	next FizzBuzzType
+}
+
+func (fizzType *Fizz) result(number float64) string {
 	if 0 == math.Mod(number, 3) {
 		return "Fizz"
 	}
+	return fizzType.next.result(number)
+}
+
+type DefaultType struct{}
+
+func (defaultType *DefaultType) result(number float64) string {
+
 	return strconv.FormatFloat(number, 'f', -1, 64)
+}
+
+var fizzBuzzTypes = &Fizz{&DefaultType{}}
+
+func FizzBuzz(number float64) string {
+	return fizzBuzzTypes.result(number)
 }
